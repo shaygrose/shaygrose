97b5361ac0e6e95dc724aa09d33041e1452a8797
Shayna <sgrose@sfu.ca>
2021-02-02 20:31:29 -0800

    Adding some styling to home page

---
+++ build/precache-manifest.5f270486967cfba9bb6ca65882fe819b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1348d362e99ffdc26c75ac0676516af1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4ada661aeb08148deed",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "426bc4d96b7b7a8b4e17",
+    "url": "/static/js/2.330e2044.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.330e2044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4ada661aeb08148deed",
+    "url": "/static/js/main.6dc912e1.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
